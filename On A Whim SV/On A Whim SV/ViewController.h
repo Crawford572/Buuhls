b9299be07924d5afda0ce44665c236b897dd8950
Corey <user@CoreyYates.local>
2014-12-08 22:48:04 -0600

    Single View

---
+++ On A Whim SV/On A Whim SV/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  On A Whim SV
+//
+//  Created by Corey on 12/8/14.
+//  Copyright (c) 2014 SoLR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
